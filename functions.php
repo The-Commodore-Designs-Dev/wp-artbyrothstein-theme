7a17da7a2011189547f018526c3b6634da5bbf05
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2023-11-15 12:05:29 -0500

    Add Tailwind and Composer

--- functions.php
+++ functions.php
@@ -1,38 +1,52 @@
 <?php
-
-function artbyrothstein_theme_support(){
-    // Adds a dynamic title from Wordpress
-    add_theme_support('title-tag');
-}
-
-add_action('after_theme_setup', 'artbyrothstein_theme_support');
-
-function artbyrothstein_menus(){
-    $locations = array(
-        'primary' => "Desktop Primary Top Header",
-        'footer' => "Footer Menu Items"
+function register_nav()
+{
+    register_nav_menus(
+        array(
+            'header' => __('Header', 'artbyrothstein')
+        )
     );
-    rester_nav_menus($locations);
+
+    register_nav_menus(
+        array(
+            'footer' => __('Footer', 'artbyrothstein')
+        )
+    );
+
+    register_nav_menus(
+        array(
+            '404' => __('404', 'artbyrothstein')
+        )
+    );
 }
 
-add_action('init', 'artbyrothstein_menus');
+if (!function_exists('setup')):
+    function setup()
+    {
+        register_nav();
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_image_size('team', 400, 400, array('center', 'center'));
+    }
+endif;
 
-function artbyrothstein_register_styles(){
-    $version = wp_get_theme() -> get( 'Version' );
-    wp_enqueue_style('artbyrothstein-style', get_template_directory_uri()."/style.css", array('artbyrothstein-bootstrap'), $version, 'all');
-    wp_enqueue_style('artbyrothstein-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
-    wp_enqueue_style('artbyrothstein-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+function boilerplate_load_assets()
+{
+    wp_enqueue_script('ourmainjs', get_theme_file_uri('/frontend/build/index.js'), array('wp-element'), '1.0', true);
+    wp_enqueue_style('ourmaincss', get_theme_file_uri('/frontend/build/index.css'));
 }
 
-add_action('wp_enqueue_scripts', 'artbyrothstein_register_styles');
-
-function artbyrothstein_register_scripts(){
-    $version = wp_get_theme() -> get( 'Version' );
-    wp_enqueue_script('artbyrothstein-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
-    wp_enqueue_script('artbyrothstein-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', true);
-    wp_enqueue_script('artbyrothstein-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
-    wp_enqueue_script('artbyrothstein-script', get_template_directory_uri()."/assets/js/main.js", $version, true);
+function scripts_header()
+{
+    wp_enqueue_style('init', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'artbyrothstein_register_scripts');
-?>
+function scripts_footer()
+{
+    //wp_enqueue_script('init', get_template_directory_uri().'/js/init.js', array('jquery'), '1.0.0', true);
+}
+
+add_action('after_setup_theme', 'setup');
+add_action('wp_enqueue_scripts', 'scripts_header');
+add_action('wp_enqueue_scripts', 'boilerplate_load_assets');
+//add_action('wp_footer', 'scripts_footer');
--- header.php
+++ header.php
@@ -1,50 +1,13 @@
 <!DOCTYPE html>
-<html lang="en"> 
-<head>
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Art By Rothstein">
-    <meta name="author" content="https://gregorypaulrothstein.dev">    
-    <link rel="shortcut icon" href="images/logo.png"> 
-     
-	<!-- Theme CSS -->
-    <?php
-    wp_head();
-    ?>
-
-</head>
-
-<body>
-    
-    <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
-        <ul class="social-list list-inline py-3 mx-auto">
-			<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
-			<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
-			<li class="list-inline-item"><a href="#"><i class="fab fa-github-alt fa-fw"></i></a></li>
-			<li class="list-inline-item"><a href="#"><i class="fab fa-stack-overflow fa-fw"></i></a></li>
-			<li class="list-inline-item"><a href="#"><i class="fab fa-codepen fa-fw"></i></a></li>
-		</ul>
-	    <nav class="navbar navbar-expand-lg navbar-dark" >
-           
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div id="navigation" class="collapse navbar-collapse flex-row" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
-				<?php
-                wp_nav_menu(
-                    array(
-                        'menu' => 'primary',
-                        'container' => '',
-                        'theme_location' => 'primary',
-                        'items_wrap' => '<ul id="" class="navbar-nav flex-row text-sm-center text-md-left">%3$s</ul>'
-                    )
-                );
-                ?>
-			</div>
-		</nav>
-    </header>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <div class="bg-gray-300">
+      <div class="max-w-4xl mx-auto mb-5 px-4">
+        <h1 class="text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500">Welcome To Our Header!</a></h1>
+      </div>
+    </div>
