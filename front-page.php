fa9ea93c6e7db99a1c8ff2cf996d018a78bb2718
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2023-11-15 14:07:27 -0500

    Work on Header

--- front-page.php
+++ front-page.php
@@ -1,27 +1,6 @@
 <?php
 get_header();
-?>  
-    <div class="main-wrapper">
-	    <header class="page-title theme-bg-light text-center gradient py-5">
-			<h1 class="heading"><?php the_title(); ?></h1>
-		</header>
-		<article class="content px-3 py-5 p-md-5">
-	    	<?php
-				if ( have_posts() ) {
-					while ( have_posts() ) {
-						the_post();
-						the_content();
-					}
-				}
-			?>
-	    </article>
-	    <footer class="footer text-center py-2 theme-bg-dark">
-		   
-            <p class="copyright"><a href="https://artbyrothstein.com">ArtByRothstein</a></p>
-		   
-	    </footer>
-    
-    </div>
+?>
     
 <?php
 get_footer();
